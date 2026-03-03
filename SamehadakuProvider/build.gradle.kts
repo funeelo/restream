cce3194ad3e60ddd7156230de8bea11d9cb3d853
funeelo <110329388+funeelo@users.noreply.github.com>
2026-03-03 19:56:47 +0700

    Build ef044dd224f8b6a307470435f68929b6d042735f

---
+++ SamehadakuProvider/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1  
+
+cloudstream {
+    language = "id"
+    description = "Samehadaku — Streaming Anime Subtitle Indonesia"
+    authors = listOf("Miku")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "OVA",
+        "Anime",
+    )
+    
+    iconUrl = "https://lh3.googleusercontent.com/zwKn_6sdYOdIiWg6Gtnx-Ar2DrVamlADnPN8BE4f84-a9aRo2Zc7mxbW5Qd26Z8cPFc"
+    
+}
